c126d2bafa5d48cf3a26a8bb94a5d30911fc70f3
vanjamilicevic <62884162+vanjamilicevic@users.noreply.github.com>
2021-09-30 15:44:58 +0200

    Enhanced mobile and added landscape

--- Utils/index.js
+++ Utils/index.js
@@ -14,33 +14,45 @@ document.getElementById('button').addEventListener('click', function() {
 
     if (!nameValue) {
         document.getElementsByClassName("hidden-warning")[0].style.visibility = "visible";
+        document.getElementById("name-input").style.borderColor = "#FF7979";
         document.getElementById("name-input").classList.remove("transparent");
+        document.getElementById("name-input").placeholder = "";
     } else {
         document.getElementsByClassName("hidden-warning")[0].style.visibility = "hidden";
+        document.getElementById("name-input").style.borderColor = "#DEDEDE";
         document.getElementById("name-input").classList.add("transparent");
     }
 
     if (!lastNameValue) {
         document.getElementsByClassName("hidden-warning")[1].style.visibility = "visible";
+        document.getElementById("last-name-input").style.borderColor = "#FF7979";
         document.getElementById("last-name-input").classList.remove("transparent");
+        document.getElementById("last-name-input").placeholder = "";
     } else {
         document.getElementsByClassName("hidden-warning")[1].style.visibility = "hidden";
+        document.getElementById("last-name-input").style.borderColor = "#DEDEDE";
         document.getElementById("last-name-input").classList.add("transparent");
     }
     
     if (!validateEmail(emailValue)) {
         document.getElementsByClassName("hidden-warning")[2].style.visibility = "visible";
+        document.getElementById("email-input").style.borderColor = "#FF7979";
         document.getElementById("email-input").classList.remove("transparent");
+        document.getElementById("email-input").placeholder = "";
     } else {
         document.getElementsByClassName("hidden-warning")[2].style.visibility = "hidden";
+        document.getElementById("email-input").style.borderColor = "#DEDEDE";
         document.getElementById("email-input").classList.add("transparent");
     }
 
     if (!passvordValue) {
         document.getElementsByClassName("hidden-warning")[3].style.visibility = "visible";
+        document.getElementById("password-input").style.borderColor = "#FF7979";
         document.getElementById("password-input").classList.remove("transparent");
+        document.getElementById("password-input").placeholder = "";
     } else {
         document.getElementsByClassName("hidden-warning")[3].style.visibility = "hidden";
+        document.getElementById("passvord-input").style.borderColor = "#DEDEDE";
         document.getElementById("password-input").classList.add("transparent");
     }
 });
